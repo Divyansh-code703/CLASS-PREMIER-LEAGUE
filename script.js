da18085977ccd83a01b138084c1cfc5db5104f4e
Divyansh-code703 <hindlekhninews1@gmail.com>
2025-12-27 19:46:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,55 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } 
-from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCdpxNsLzKNeZ9MhQqU_T_oLdg-hCoXzSk",
   authDomain: "class-premier-league.firebaseapp.com",
+  databaseURL: "https://class-premier-league-default-rtdb.firebaseio.com",
   projectId: "class-premier-league",
   storageBucket: "class-premier-league.firebasestorage.app",
   messagingSenderId: "59210532535",
   appId: "1:59210532535:web:4558b69e94949b65cc6f32"
 };
 
-initializeApp(firebaseConfig);
-const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
 
-/* ELEMENTS */
 const authBox = document.getElementById("authBox");
-const teamSelect = document.getElementById("teamSelect");
+const teamBox = document.getElementById("teamBox");
 const dashboard = document.getElementById("dashboard");
-const authMsg = document.getElementById("authMsg");
+const msg = document.getElementById("msg");
+const teamMsg = document.getElementById("teamMsg");
 
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+window.signup = () =>{
+  createUserWithEmailAndPassword(auth,email.value,password.value)
+  .catch(e=>msg.innerText=e.message);
+}
 
-document.getElementById("loginBtn").onclick = () => {
-  if (!email.value || !password.value) {
-    authMsg.innerText = "Fill email & password";
-    return;
-  }
+window.login = () =>{
+  signInWithEmailAndPassword(auth,email.value,password.value)
+  .catch(()=>msg.innerText="Account not found / wrong password");
+}
 
-  signInWithEmailAndPassword(auth, email.value, password.value)
-    .then(() => {
-      authBox.classList.add("hidden");
-      teamSelect.classList.remove("hidden");
-    })
-    .catch(() => {
-      authMsg.innerText = "Account not found";
-    });
-};
-
-document.getElementById("signupBtn").onclick = () => {
-  if (!email.value || !password.value) {
-    authMsg.innerText = "Fill email & password";
-    return;
-  }
-
-  createUserWithEmailAndPassword(auth, email.value, password.value)
-    .then(() => {
-      authBox.classList.add("hidden");
-      teamSelect.classList.remove("hidden");
-    })
-    .catch(() => {
-      authMsg.innerText = "Account already exists";
-    });
-};
-
-/* TEAM SELECT */
-const teamsDiv = document.getElementById("teams");
-const teamImgs = [
-  "chennai-super-kings3461.jpg",
-  "1200px-Mumbai_Indians_Logo.svg (1).png",
-  "250px-Royal_Challengers_Bengaluru_Logo.svg.png",
-  "778px-Kolkata_Knight_Riders_Logo.svg.pn",
-  "627d11598a632ca996477eb0.png",
-  "Punjab_Kings_Logo.svg.png",
-  "627d09228a632ca996477e87 (1).png",
-  "1200px-Lucknow_Super_Giants_IPL_Logo.svg (1).png"
-];
-
-teamImgs.forEach(imgName => {
-  const img = document.createElement("img");
-  img.src = imgName;
-  img.onclick = () => {
-    teamSelect.classList.add("hidden");
-    dashboard.classList.remove("hidden");
-    document.getElementById("myTeam").innerText = "Team Selected";
-  };
-  teamsDiv.appendChild(img);
+onAuthStateChanged(auth,(user)=>{
+  if(!user) return;
+  get(ref(db,"users/"+user.uid)).then(snap=>{
+    authBox.classList.add("hidden");
+    snap.exists() ? dashboard.classList.remove("hidden") : teamBox.classList.remove("hidden");
+  });
 });
+
+window.selectTeam = (team)=>{
+  const uid = auth.currentUser.uid;
+  get(ref(db,"teams/"+team)).then(snap=>{
+    if(snap.exists()){
+      teamMsg.innerText="❌ Team already taken";
+    }else{
+      set(ref(db,"teams/"+team),uid);
+      set(ref(db,"users/"+uid),{team});
+      teamBox.classList.add("hidden");
+      dashboard.classList.remove("hidden");
+    }
+  });
+  }
