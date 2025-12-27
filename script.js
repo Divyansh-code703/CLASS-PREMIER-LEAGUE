1a629400cdf4b2eee1d7bdda5c3feec1486bb133
Divyansh-code703 <hindlekhninews1@gmail.com>
2025-12-27 20:20:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-// 🔥 FIREBASE SETUP
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
-import { getDatabase, ref, get, set } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js";
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { 
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import {
+  getDatabase,
+  ref,
+  get,
+  set
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCdpxNsLzKNeZ9MhQqU_T_oLdg-hCoXzSk",
   authDomain: "class-premier-league.firebaseapp.com",
   databaseURL: "https://class-premier-league-default-rtdb.firebaseio.com",
-  projectId: "class-premier-league",
+  projectId: "class-premier-league"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -22,85 +32,101 @@ const authMsg = document.getElementById("authMsg");
 const teamMsg = document.getElementById("teamMsg");
 const teamName = document.getElementById("teamName");
 
-// AUTH FUNCTIONS
-window.signup = () =>{
-  const email = email.value;
-  const pass = password.value;
+// 🔐 AUTH STATE CONTROLLER (IMPORTANT)
+onAuthStateChanged(auth, user => {
 
-  if(!email || !pass){
-    authMsg.innerText = "Fill all fields";
+  // sab hide pehle
+  authBox.classList.add("hidden");
+  teamBox.classList.add("hidden");
+  dashBox.classList.add("hidden");
+
+  if (!user) {
+    // user logged out
+    authBox.classList.remove("hidden");
     return;
   }
 
-  createUserWithEmailAndPassword(auth,email,pass)
-  .catch(err=>{
-    if(err.code==="auth/email-already-in-use"){
-      authMsg.innerText="Account exists, login";
-    }else{
-      authMsg.innerText="Signup error";
-    }
-  });
-};
-
-window.login = () =>{
-  const email = document.getElementById("email").value;
-  const pass = document.getElementById("password").value;
-
-  signInWithEmailAndPassword(auth,email,pass)
-  .catch(err=>{
-    if(err.code==="auth/user-not-found"){
-      authMsg.innerText="Signup first";
-    }else if(err.code==="auth/wrong-password"){
-      authMsg.innerText="Wrong password";
-    }else{
-      authMsg.innerText="Login error";
-    }
-  });
-};
-
-// SESSION CHECK
-onAuthStateChanged(auth,user=>{
-  if(user){
-    authBox.classList.add("hidden");
-    checkTeam(user.uid);
-  }else{
-    authBox.classList.remove("hidden");
-  }
+  // user logged in
+  checkTeam(user.uid);
 });
 
-// TEAM LOGIC
-function checkTeam(uid){
-  get(ref(db,"users/"+uid)).then(snap=>{
-    if(snap.exists()){
+// SIGNUP
+window.signup = () => {
+  const emailVal = document.getElementById("email").value;
+  const passVal = document.getElementById("password").value;
+
+  if (!emailVal || !passVal) {
+    authMsg.innerText = "Please fill all fields";
+    return;
+  }
+
+  createUserWithEmailAndPassword(auth, emailVal, passVal)
+    .catch(err => {
+      if (err.code === "auth/email-already-in-use") {
+        authMsg.innerText = "Account exists, login";
+      } else {
+        authMsg.innerText = "Signup error";
+      }
+    });
+};
+
+// LOGIN
+window.login = () => {
+  const emailVal = document.getElementById("email").value;
+  const passVal = document.getElementById("password").value;
+
+  if (!emailVal || !passVal) {
+    authMsg.innerText = "Please fill all fields";
+    return;
+  }
+
+  signInWithEmailAndPassword(auth, emailVal, passVal)
+    .catch(err => {
+      if (err.code === "auth/user-not-found") {
+        authMsg.innerText = "Signup first";
+      } else if (err.code === "auth/wrong-password") {
+        authMsg.innerText = "Wrong password";
+      } else {
+        authMsg.innerText = "Login error";
+      }
+    });
+};
+
+// TEAM CHECK
+function checkTeam(uid) {
+  get(ref(db, "users/" + uid)).then(snap => {
+    if (snap.exists()) {
       showDashboard(snap.val().team);
-    }else{
+    } else {
       teamBox.classList.remove("hidden");
     }
   });
 }
 
-window.selectTeam = (team)=>{
+// TEAM SELECT
+window.selectTeam = team => {
   const uid = auth.currentUser.uid;
-  const teamRef = ref(db,"teams/"+team);
+  const teamRef = ref(db, "teams/" + team);
 
-  get(teamRef).then(snap=>{
-    if(snap.exists()){
-      teamMsg.innerText="Team already taken";
-    }else{
-      set(teamRef,{takenBy:uid});
-      set(ref(db,"users/"+uid),{team});
+  get(teamRef).then(snap => {
+    if (snap.exists()) {
+      teamMsg.innerText = "Team already taken";
+    } else {
+      set(teamRef, { takenBy: uid });
+      set(ref(db, "users/" + uid), { team });
       showDashboard(team);
     }
   });
 };
 
-function showDashboard(team){
+// DASHBOARD
+function showDashboard(team) {
   teamBox.classList.add("hidden");
   dashBox.classList.remove("hidden");
-  teamName.innerText="Your Team: "+team;
+  teamName.innerText = "Your Team: " + team;
 }
 
-window.logout = ()=>{
+// LOGOUT
+window.logout = () => {
   signOut(auth);
-  location.reload();
 };
